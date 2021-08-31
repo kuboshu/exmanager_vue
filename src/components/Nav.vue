2271fbde68d09142573a2c64c946f03ff02b0049
kuboshu83 <kuboshu83@gmail.com>
2021-08-31 22:55:59 +0900

    [update]トップページからログインとユーザー登録に遷移できるようにした。

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,17 @@
 <template>
     <nav>
-        <button v-for="menu in menus" v-bind:key="menu">{{menu}}</button>
+        <button v-for="menu in menus" v-on:click="changePage(menu[1])" v-bind:key="menu">{{menu[0]}}</button>
     </nav>
 </template>
 
 <script>
 export default ({
     name: 'Nav',
-    data: function() {
-        return {
-            menus: ['ログイン', 'ユーザー登録']
-        };
+    props: ['menus'],
+    methods: {
+        changePage: function(path) {
+            this.$router.push({path: path});
+        }
     }
 });
 </script>
--- src/components/TopPage.vue
+++ src/components/TopPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Nav></Nav>
+    <Nav v-bind:menus="menus"></Nav>
     <h1>レポートを共有しようぜぃ!!</h1>
     <Search></Search>
     最新記事2020/1/1更新
@@ -13,12 +13,22 @@ import Nav from './Nav.vue'
 import Search from './Search.vue'
 import News from './News.vue'
 
+const menus = [
+  ['ログイン', "login"],
+  ['ユーザー登録', 'reg'],
+];
+
 export default {
   name: 'TopPage',
   components: {
     'Nav': Nav,
     'Search': Search,
     'News': News
+  },
+  data: function () {
+    return {
+      menus: menus
+    };
   }
 }
 </script>
