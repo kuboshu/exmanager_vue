a988a7008d66892a9b281297c4db0f4bc1544a52
kuboshu83 <kuboshu83@gmail.com>
2021-08-31 19:34:53 +0900

    [update]ルーティングの基礎部分作成

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+import TopPage from './components/TopPage.vue'
+import Login from './components/Login.vue'
+
+const routes = [
+    {path: '/', component: TopPage},
+    {path: '/login', component: Login},
+];
+
+const router = createRouter({
+    history: createWebHashHistory(process.env.BASE_URL),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
