25288804fffa3bd1f3a0f094e59a8ad5068b61a1
kuboshu83 <kuboshu83@gmail.com>
2021-09-01 21:27:07 +0900

    [update]認証はできるようにした。

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,9 +1,55 @@
 <template>
-    <h1>ユーザー認証</h1>
+    <div>
+        <h1>ユーザー認証</h1>
+        <p v-if="fail_auth" style="color: red;">認証に失敗しました</p>
+        <div>
+            アカウント名:
+            <input type="text" placeholder="アカウント名" v-model="name">
+        </div>
+
+        <div>
+            パスワード:
+            <input type="password" placeholder="パスワード" v-model="passwd">
+        </div>
+
+        <div>
+            <button @click="certification">認証</button>
+            <button @click="toTopPage">キャンセル</button>
+        </div>
+    </div>
 </template>
 
 <script>
+const users = new Map([
+    ['akira', 'akira_passwd'],
+    ['akari', 'akari_passwd'],
+    ['aika', 'aika_passwd'],
+    ['alice', 'alice_passwd'],
+]);
+
 export default ({
-    name: 'Login'
+    name: 'Login',
+
+    data: function() {
+        return {
+            name: '',
+            passwd: '',
+            fail_auth: false
+        };
+    },
+
+    methods: {
+        certification: function() {
+            if ((users.has(this.name)) && (users.get(this.name) == this.passwd)) {
+                this.$router.push("usertop");
+            } else {
+                this.fail_auth = true;
+            }
+        },
+
+        toTopPage: function() {
+            this.$router.push("/");
+        }
+    }
 });
 </script>
